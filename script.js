e8565c641c9dd7930f5254bb04d7915673f51d33
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-01-16 10:38:47 +0700

    create function javasript

---
+++ script.js
@@ -0,0 +1,6 @@
+const search = () => {
+    const searchbox = document.getElementById("search").value.toUppercase();
+    const productItem = document.getElementsByClassName("product-list")[0];
+    const product = document.querySelectorAll(".product");
+    const h2Name = document.getElementsByTagName("h2");
+}
