9b72e1544010db6ad94c61714b451cc8c32a797a
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-01-16 11:48:23 +0700

    not clear

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const search = () => {
-    const searchbox = document.getElementById("search").value.toUppercase();
+    const searchbox = document.getElementById("search").value.toUpperCase();
     const productItem = document.getElementsByClassName("product-list")[0];
     const product = document.querySelectorAll(".product");
-    const h2Name = document.getElementsByTagName("h2");
+    const h2Name = productItem.getElementsByTagName("h2");
+
+
+    for (let index = 0; index < h2Name.length; index++) {
+        let match = product[index].getElementsByTagName("h2")[0];
+
+        if (match) {
+            let textValue = match.textContent || match.innerHTML
+
+            if (textValue.toUpperCase().indexOf(searchbox) > -1) {
+                product[index].style.display = " ";
+            } else {
+                product[index].style.display = "none";
+            }
+        }
+    }
 }
+
+search()
